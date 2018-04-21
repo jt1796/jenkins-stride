301621b61f079a64d4190a9adc24d7def1a90077
John Tompkins <tompkins.jt@gmail.com>
2018-04-21 18:10:14 -0400

    pushes notifications to stride

--- src/main/java/jt/stride/StridePublisher.java
+++ src/main/java/jt/stride/StridePublisher.java
@@ -9,7 +9,12 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
 
 public class StridePublisher extends Notifier {
 
@@ -26,11 +31,47 @@ public class StridePublisher extends Notifier {
     }
 
     @Override
-    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        listener.getLogger().print("I am running!");
+    public boolean needsToRunAfterFinalized() {
         return true;
     }
-    
+
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        //TODO: parameterize the conversation id in the build.
+        //TODO: URL should be configured in global settings.
+        listener.getLogger().println("Sending to Stride");
+        HttpClient client = new HttpClient();
+        PostMethod post = new PostMethod("https://api.atlassian.com/site/f96cf489-a553-4645-af11-cb766cb85c23/conversation/b3b26d80-3f72-4aec-bdf1-68976238c74f/message");
+        String body = buildMessage(build);
+        post.setRequestBody(body);
+        post.addRequestHeader(new Header("Content-Type", "application/json"));
+        post.addRequestHeader(new Header("Authorization", "Bearer " + StrideConfiguration.get().getAccessToken()));
+        try {
+            int statusCode = client.executeMethod(post);
+            if (statusCode >= 400) {
+                listener.getLogger().println("Error sending message to stride, received a " + statusCode + " status code.");
+                return false;
+            }
+        } catch(IOException e) {
+            listener.getLogger().println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private String buildMessage(AbstractBuild build) {
+        //TODO: Json could be made invalid, sanitize this.sendMsg
+        //TODO: This isn't really the build status just something good enough
+        String msg = this.sendMsg;
+        msg = msg.replaceAll("\\$BUILD_NUMBER", "" + build.number);
+        msg = msg.replaceAll("\\$BUILD_NAME", build.getParent().getDisplayName());
+        msg = msg.replaceAll("\\$BUILD_STATUS", build.getBuildStatusSummary().message);
+        msg = "{\"body\":{\"version\":1,\"type\":\"doc\",\"content\":[{\"type\":\"paragraph\",\"content\":[{\"type\":\"text\",\"text\":\""
+                + msg
+                + "\"}]}]}}";
+        return msg;
+    }
+
     public boolean isSendOnStart() {
         return sendOnStart;
     }
